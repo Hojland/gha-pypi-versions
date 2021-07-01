35dbaf3654756a062abf266f697aeb96d8f84eb5
Martin Hansen <molheh@nuuday.dk>
2021-07-01 15:01:18 +0200

    input test

--- version.py
+++ version.py
@@ -6,12 +6,10 @@ def versions(package_name: str, latest_num: int):
     url = f"https://pypi.org/pypi/{package_name}/json"
     data = requests.get(url).json()
     versions = list(data["releases"].keys())
-    print(versions)
     if len(versions) > latest_num:
         versions = versions[-latest_num:]
     return versions
 
 if __name__ == "__main__":
-    print(sys.argv[1])
     vers = versions(str(sys.argv[1]), int(sys.argv[2]))
     print(f"::set-output name=versions::${vers}")
