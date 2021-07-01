2120fdfb6f0f7de0e860b5d0a47bdf4d7d243938
Martin Hansen <molheh@nuuday.dk>
2021-07-01 14:01:12 +0200

    print version

--- version.py
+++ version.py
@@ -5,7 +5,10 @@ from distutils.version import StrictVersion
 def versions(package_name: str, latest_num: int):
     url = f"https://pypi.org/pypi/{package_name}/json"
     data = requests.get(url).json()
-    versions = list(data["releases"].keys())[-latest_num:]
+    versions = list(data["releases"].keys())
+    print(versions)
+    if len(versions) > latest_num:
+        versions = versions[-latest_num:]
     return versions
 
 if __name__ == "__main__":
